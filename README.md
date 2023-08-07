e9a4b9fe94ecd189c0c0ab19877c4d162db3f7d3
zfb132 <zfb132@gmail.com>
2023-08-07 21:07:38 +0800

    add feat: ieee-tap

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# check_submission_status
+# 使用说明
+本程序会自动登录`Scholar One`投稿系统，获取当前稿件的状态，并将状态（若状态发生变化）发送到指定邮箱  
+可以配合`crontab`或者`timer`定时运行，实现自动化检测稿件状态的功能  
+
+## 1. 安装谷歌浏览器
+Windows直接下载安装`exe`文件即可  
+Linux系统下，直接下载`deb`文件安装即可
+
+## 2. 安装谷歌浏览器驱动
+[点击这里下载](https://chromedriver.chromium.org/downloads)  
+根据提示下载对应版本的驱动（注意谷歌浏览器版本号、操作系统类型必须匹配）压缩包，解压出可执行文件`chromedriver(.exe)`
+
+## 3. 检测是否安装成功
+```bash
+chromedriver --version
+
+# ChromeDriver 115.0.5790.170 (cc0d30c2ca5577520c8646671513241faa0bc105-refs/branch-heads/5790@{#1923})
+```
+
+## 4. 安装依赖
+```bash
+pip install bs4 splinter selenium lxml
+```
+
+## 5. 修改配置
+重命名`config.py`文件为`config.example.py`文件，修改其中的配置项
+
+## 6. 运行
+```bash
+python3 main.py
+```
---
+++ main.py
@@ -0,0 +1,114 @@
+import os
+import random
+import smtplib
+import time
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from splinter import Browser
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+
+from email.mime.text import MIMEText
+from email.header import Header
+from log import initLog
+from config import *
+
+logging = initLog('submission.log', __name__)
+
+def send_email(msg):
+    message = MIMEText(f'New status for {URL}: \n{msg}', 'plain', 'utf-8')
+    message['From'] = Header("Status Monitoring Kit", 'utf-8')
+    message['To'] =  Header("userName", 'utf-8')
+    subject = URL.split('/')[-1].upper()
+    message['Subject'] = Header(f'{subject} Manuscript Status: {msg}', 'utf-8')
+    try:
+        smtpObj = smtplib.SMTP() 
+        smtpObj.connect(SENDER_SERVER, 587)    # 587 为 SMTP 端口号，
+        smtpObj.login(SENDER_EMAIL, SENDER_PWD)  
+        smtpObj.sendmail(SENDER_EMAIL, RECEIVERS, message.as_string())
+        print("Sent email successfully")
+        logging.info("Sent email successfully")
+    except smtplib.SMTPException:
+        print("Error: unable to send email")
+        logging.error("Error: unable to send email")
+
+def get_random_interval():
+    '''
+    获取随机时间间隔
+    '''
+    return random.randint(1, 5)
+
+
+def update_status(status):
+    '''
+    更新投稿状态
+    '''
+    name = "status.txt"
+    if not os.path.exists(name):
+        with open(name, 'w') as f:
+            f.write(status)
+            logging.info(f'Crate {name} file: current status is "{status}"')
+    else:
+        with open(name, 'r') as f:
+            previous_status = f.read()
+            if previous_status != status:
+                with open(name, 'w') as f:
+                    f.write(status)
+                    logging.info(f'Update {name} file: current status is "{status}"')
+                    send_email(status)
+            else:
+                logging.info(f'Current status is "{status}", no update')
+
+
+def run():
+    '''
+    登录投稿网站，获取投稿状态
+    '''
+    # 指定chromedriver路径
+    my_service = Service(executable_path=DRIVER_PATH)
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36")
+    chrome_options.add_argument('--disable-blink-features=AutomationControlled')
+    chrome_options.add_argument("--disable-extensions")
+    chrome_options.add_argument("--disable-gpu")
+    chrome_options.add_argument("--headless")
+    chrome_options.add_experimental_option('useAutomationExtension', False)
+    chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
+    b = Browser(driver_name='chrome', service=my_service, options=chrome_options)
+    time.sleep(get_random_interval())
+    b.visit(URL)
+    time.sleep(get_random_interval())
+    b.fill('USERID', USER)
+    time.sleep(get_random_interval())
+    b.fill('PASSWORD', PWD)
+    time.sleep(get_random_interval())
+    # 点击登录按钮
+    wait = WebDriverWait(b.driver, 30)
+    e = wait.until(EC.element_to_be_clickable((By.ID, 'logInButton')))
+    b.driver.execute_script("arguments[0].click();", e)
+    time.sleep(get_random_interval())
+    # 切换到作者页面
+    html_obj = b.html
+    soup = BeautifulSoup(html_obj,"lxml")
+    li = soup.find_all("li", attrs={"class": "nav-link"})
+    for a in li:
+        author_tag = a.find("a")
+        if author_tag and author_tag.text.strip() == "Author":
+            b.driver.execute_script(author_tag["href"].split("javascript:")[-1])
+            break
+    time.sleep(get_random_interval())
+    html_obj = b.html
+    soup = BeautifulSoup(html_obj,"lxml")
+    table = soup.find("span", attrs={"class": "pagecontents"})
+    print(table.string)
+    current_manuscript_status = table.string
+    time.sleep(get_random_interval())
+    b.quit()
+    update_status(current_manuscript_status)
+
+
+if __name__=="__main__":
+    run()
